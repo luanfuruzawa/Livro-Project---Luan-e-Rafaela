c9e3b3e41fda2270bc4b865dddcbec33fe3d8bfa
Rafaela <rafaela.fantini01@gmail.com>
2026-08-23 12:51:10 -0300

    Correção pela metade da chave primária

--- project-livro/database/migrations/0001_01_01_000000_create_users_table.php
+++ project-livro/database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,15 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('email')->primary();  
+            $table->id()->primary();
+            $table->string('email')->unique();
             $table->string('username');
             $table->string('nivel_acesso');
             $table->string('password');

f2d3df0d4cc4320d5f7d90dc8a1aa2de3e57c15a
luanfuruzawa <20241ctb0100006@estudantes.ifpr.edu.br>
2026-08-23 16:08:32 -0300

    remoção de livros e refatoração de controller e metodo de verificação melhorado

---
+++ project-livro/resources/views/componentes/botao-editar.blade.php
@@ -0,0 +1,4 @@
+<button type="button" class="btn btn-outline-dark btn-sm d-inline-flex align-items-center px-3 py-2 fw-semibold">
+    <i class="fa-solid fa-pen-to-square me-2"></i>
+    Editar
+</button>
